e7bdbab64c08b04ce66830b8fe779f81045c75d0
RikelmVoltz <142436269+RikelmVoltz@users.noreply.github.com>
2023-08-24 17:00:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,78 @@
+
+// rikelme voltz
+
+//A
+
+//resposta 1)
+// A)false B)false C)true D)bollean
+
+
+
+//resposta 2:
+//sim,ele criou outra variavel com o nome "soma" e "deu console.log"
+
+
+//resposta 3 :
+// para que o codigo funcione do jeito certo seria  "console.log(primeiroNumero + segundoNumero);"
+
+//B
+
+// ATVD 1 
+/*
+let idade = prompt("quantos anos você tem?");
+console.log("você tem", idade);
+
+
+
+let perguntaBff = prompt("quantos anos seu melhor amigo tem?");
+console.log("seu melhor amigo tem", perguntaBff);
+
+
+let idadeBff = 16;
+let minhaIdade = 16;'
+console.log("Sua idade é maior do que a do seu amigo?", minhaIdade > idadeBff);
+*/
+
+// ATVD 2
+/*
+let numero = 24;
+let pergunta = prompt("digite um numero par");
+console.log(numero % 4);
+
+sim o padrão foi resultar em 0 todos os resto da divisão com o números pares
+
+
+se colocar um número impar o resultado sempre será 1
+
+*/
+/*
+let anosIdade = prompt("digite sua idade em anos"); 
+let anos = ("qual a sua idade em anos?", anosIdade);
+console.log("quantos anos você tem?", anosIdade)
+let idadeD = Number(anosIdade * 12);
+console.log("idade em meses", idadeD);  
+let idadeDias = Number(365 * idadeD);
+console.log("idade em dias", idadeDias); 
+let Idade2 = Number(idadeDias * 24);
+console.log("sua idade em horas", Idade2)
+
+
+
+// ATVD 4 
+
+
+
+let numero = 22;
+let number = 44; 
+
+let primeiroNumero = prompt("digite um numero");
+let segundoNumero = prompt("digite outro numero");
+console.log("você digitou", primeiroNumero);
+console.log("você digitou", segundoNumero);
+
+console.log("primeiro numero é maior que o segundo?", primeiroNumero > segundoNumero);
+console.log("primeiro numero é igual o segundo?", primeiroNumero >= segundoNumero);
+console.log( "primeiro numero é divisivel pelo segundo?", Boolean(primeiroNumero / segundoNumero));
+console.log("segundo numero é divisel pelo primeiro?", Boolean(segundoNumero / primeiroNumero ));
+*/
+
